ef8e6f96b1b6005c70b7a54a6085f554731be072
PaxNotFun <96891781+PaxNotFun@users.noreply.github.com>
2024-01-08 16:37:07 -0600

    Update holaclient.sh

--- holaclient.sh
+++ holaclient.sh
@@ -13,9 +13,7 @@ apt install unzip -y
 echo "Estamos Instalando Todo Espera Por Favor " | boxes -d peek -a c -s 40x11
 echo "Descargando Panel" | boxes -d peek -a c -s 40x1
 # Clone HolaClient repository
-rm -rf HolaClient
-rm -rf HolaClient.zip
-git clone https://github.com/HolaClient/HolaClient
+git clone https://github.com/OpenHeliactyl/Heliactyl.git /var/www/heliactyl
 
 echo "Configurando Firewall" | boxes -d peek -a c -s 40x1
 # Install SSL and Firewall
@@ -38,10 +36,8 @@ curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
 apt-get install nodejs gcc g++ make -y
 
 # Navigate to HolaClient directory
-cd HolaClient
-rm -rf .env
-wget https://raw.githubusercontent.com/PaxNotFun/shell/main/.env
-
+cd /var/www/heliactyl
+rm -rf settings.json
 # Install Yarn
 sudo apt-get update
 sudo apt-get install yarn -y
@@ -53,7 +49,7 @@ npm i
 npm i -g pm2
 
 # Start HolaClient with pm2
-pm2 start --name "holaclient" index.js
+pm2 start --name "Heliactyl" index.js
 
 # Configure pm2 to start on system boot
 pm2 startup
@@ -64,32 +60,29 @@ echo "Configurando NGINX" | boxes -d peek -a c -s 40x1
 echo "server {
     listen 80;
     server_name portal.cometrakko.com;
-    return 301 https://\$server_name\$request_uri;
+    return 301 https://$server_name$request_uri;
 }
-
 server {
     listen 443 ssl http2;
-
-    location /afkwspath {
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection \"upgrade\";
-        proxy_pass \"http://68.233.127.117:2000/afkwspath\";
-    }
-
+location /afkwspath {
+  proxy_http_version 1.1;
+  proxy_set_header Upgrade $http_upgrade;
+  proxy_set_header Connection "upgrade";
+  proxy_pass "http://localhost:2000/afkwspath";
+}
+    
     server_name portal.cometrakko.com;
-    ssl_certificate /etc/letsencrypt/live/portal.cometrakko.com/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/portal.cometrakko.com/privkey.pem;
+ssl_certificate /etc/letsencrypt/live/<domain>/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/<domain>/privkey.pem;
     ssl_session_cache shared:SSL:10m;
     ssl_protocols SSLv3 TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers HIGH:!aNULL:!MD5;
+    ssl_ciphers  HIGH:!aNULL:!MD5;
     ssl_prefer_server_ciphers on;
-
-    location / {
-        proxy_pass http://68.233.127.117:2000/;
-        proxy_buffering off;
-        proxy_set_header X-Real-IP \$remote_addr;
-    }
-}" | sudo tee /etc/nginx/sites-enabled/holaclient.conf
+location / {
+      proxy_pass http://localhost:2000/;
+      proxy_buffering off;
+      proxy_set_header X-Real-IP $remote_addr;
+  }
+}" | sudo tee /etc/nginx/sites-enabled/heliactyl.conf
 sudo nginx -t
 sudo systemctl reload nginx
