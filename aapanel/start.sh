ea79ae96b955fd16d138ebe2c8e7f2e917989456
PaxNotFun <96891781+PaxNotFun@users.noreply.github.com>
2024-01-09 17:17:03 -0600

    Update start.sh

--- aapanel/start.sh
+++ aapanel/start.sh
@@ -11,12 +11,11 @@ echo "Estamos Instalando Todo Espera Por Favor " | boxes -d peek -a c -s 40x11
 echo "Configurando Firewall" | boxes -d peek -a c -s 40x1
 # Install SSL and Firewall
 apt install firewalld -y
-firewall-cmd --zone=public --add-port=8080/tcp --permanent 
-firewall-cmd --zone=public --add-port=2022/tcp --permanent
+firewall-cmd --zone=public --add-port=888/tcp --permanent 
 firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --zone=public --add-port=443/tcp --permanent 
+firewall-cmd --zone=public --add-port=443/tcp --permanent
+firewall-cmd --zone=public --add-port=20/tcp --permanent 
 firewall-cmd --zone=public --add-port=21/tcp --permanent
-firewall-cmd --zone=public --add-port=22/tcp --permanent
 firewall-cmd --reload
 
 wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh aapanel
