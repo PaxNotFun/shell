64beed90d14940dedf50224854a48de0b496f839
PaxNotFun <96891781+PaxNotFun@users.noreply.github.com>
2025-01-30 16:48:33 -0600

    Update start.sh

--- jexactyl/start.sh
+++ jexactyl/start.sh
@@ -13,11 +13,11 @@ apt install nano -y
 apt install boxes -y
 apt install unzip -y
 apt install cron -y
+apt install firewalld -y
 echo "Estamos Instalando Todo Espera Por Favor " | boxes -d peek -a c -s 40x11
 
 echo "Configurando Firewall" | boxes -d peek -a c -s 40x1
 # Install SSL and Firewall
-apt install firewalld -y
 firewall-cmd --zone=public --add-port=8080/tcp --permanent 
 firewall-cmd --zone=public --add-port=2022/tcp --permanent
 firewall-cmd --zone=public --add-port=80/tcp --permanent
@@ -28,7 +28,7 @@ firewall-cmd --reload
 
 echo "Configurando SSl" | boxes -d peek -a c -s 40x1
 sudo apt install -y python3-certbot-nginx nginx
-certbot certonly --nginx -d console.cometrakko.com
+certbot certonly --nginx -d control.cometrakko.com
 
 echo "Instalando y Configurando Dependencias" | boxes -d peek -a c -s 40x1
 apt install software-properties-common curl apt-transport-https ca-certificates gnupg -y
