2e6a46925cf805483062fb59d516e12f5ab76aee
PaxNotFun <96891781+PaxNotFun@users.noreply.github.com>
2025-01-30 18:24:18 -0600

    Update start2.sh

--- jexactyl/start2.sh
+++ jexactyl/start2.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Update and upgrade system packages
-apt update
-apt upgrade -y
-
-# Instalando Dependencias
-mkdir /var/www/
-apt install git -y
-apt install nano -y
-apt install boxes -y
-apt install unzip -y
-apt install cron -y
 apt install firewalld -y
 echo "Estamos Instalando Todo Espera Por Favor " | boxes -d peek -a c -s 40x11
 
@@ -23,7 +11,3 @@ firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --zone=public --add-port=21/tcp --permanent
 firewall-cmd --zone=public --add-port=22/tcp --permanent
 firewall-cmd --reload
-
-echo "Configurando SSl" | boxes -d peek -a c -s 40x1
-sudo apt install -y python3-certbot-nginx nginx
-certbot certonly --nginx -d control.cometrakko.com
